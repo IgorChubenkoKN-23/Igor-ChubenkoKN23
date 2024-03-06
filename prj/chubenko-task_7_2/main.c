642d0169092eb04c49933bac3d4c6155c004fd5b
Igor Chubenko <igorchubenkogit@gmail.com>
2024-03-06 10:27:32 +0200

    add readme

--- prj/chubenko-task_7_2/main.c
+++ prj/chubenko-task_7_2/main.c
@@ -7,39 +7,34 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     system("chcp 65001 & cls");
-  int arr[14];
-  int i, j, temp;
+    int arr[14];
+    int i, j, temp;
 
+    printf("Введіть масив розміром 14: ");
+    for (i = 0; i < 14; i++) {
+        printf("Елемент %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
 
-  printf("Введіть масив розміром 14: ");
-  for (i = 0; i < 14; i++) {
-    scanf("%d", &arr[i]);
-  }
+    for (i = 0, j = 13; i < j; i++, j--) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 
+    printf("Результати оберненого масиву:\n");
+    for (i = 0; i < 14; i++) {
+        printf("%d ", arr[i]);
+    }
 
-  for (i = 0, j = 13; i < j; i++, j--) {
-    temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
+    int sum = 0;
+    for (i = 11; i <= 13; i++) {
+        sum += arr[i];
+    }
 
+    printf("\nСума трьох останніх елементів: %d\n", sum);
 
-  for (i = 0; i < 14; i++) {
-    printf("%d ", arr[i]);
-  }
-
-
-  int sum = 0;
-  for (i = 11; i <= 13; i++) {
-    sum += arr[i];
-  }
-
-
-  printf("\nСума трьох останніх елементів: %d\n", sum);
-
-
-  scanf("%*c");
-  printf("Натисніть Enter для завершення");
-getch(0);
-  return 0;
+    printf("Натисніть Enter для завершення");
+    getch();
+    return 0;
 }
